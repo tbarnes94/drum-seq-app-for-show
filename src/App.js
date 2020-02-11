2334ff3a0d1919737c41cd8ca4395fa79f2e47c8
Thomas Barnes <thomas.jonathan.barnes@gmail.com>
2020-02-11 01:48:22 -0500

    Added several components and one sound.  Working on play/stop functionality at 120 bpm.  Then adding more sounds.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Sequencer from './Sequencer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Drum Sequencer</h1>
+      <div className="App-body">
+        <Sequencer></Sequencer>
+      </div>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
     </div>
   );
 }
